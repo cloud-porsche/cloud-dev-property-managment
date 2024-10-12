24e0c1207a2390a9f0652407899037df9d14cda3
lars buerger <lars.buerger@freenet.de>
2024-10-12 12:51:15 +0200

    feat: adds cors suppor to fix error on frontend

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true });
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
